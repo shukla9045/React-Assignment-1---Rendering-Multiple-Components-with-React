2c1ef99bb5261ac6d7d297afac0c2c2f64653011
shukla9045 <73607833+shukla9045@users.noreply.github.com>
2021-02-08 18:11:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,14 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    //<div id="main"></div>
+    <p data-ns-test="project-name">
+    Electricity bill mgmt system
+    </p>
+    <p data-ns-test="project-description">
+    prepaid electricity bill mgmt system it provides an uniuqe like mob number you can enter on that portal and access ypour details.
+    </p>
+    
   )
 }
 
