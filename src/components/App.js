ced62cb4e8977ac368cba914da0a16296a2bfec2
shukla9045 <saurabhshukla9045@gmail.com>
2021-02-09 17:20:19 +0530

    done assign2

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+function htmltag(props) {
+  return <p>Hello, {props.data-ns-test}</p>;
+}
+
+const element1 = <htmltag data-ns-test ="project-name">electricity bill mgmt system</htmltag>
+const element2 = <htmltag data-ns-test ="project-description">it is a prepaid electricity bill mgmt system using html css javascript</htmltag>
+ 
 const App = () => {
   return (
-    //<div id="main"></div>
-    <p data-ns-test="project-name">
-    Electricity bill mgmt system
-    </p>
-    <p data-ns-test="project-description">
-    prepaid electricity bill mgmt system it provides an uniuqe like mob number you can enter on that portal and access ypour details.
-    </p>
+    // {element1,element2}
+    //<div>main</div>
+    <>
+    {element1}
     
+    {element2}
+   
+    </>
   )
 }
 
